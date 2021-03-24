e43b67d3041d90e66cec0b883f74337ee3f81330
Bruno Faboci <brunofaboci@gmail.com>
2021-03-24 11:45:55 -0300

    add links sign_in / sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,20 @@ Rails.application.routes.draw do
   namespace :users_backoffice do
     get 'welcome/index'
   end
+
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :users
-  devise_for :admins
+
   namespace :site do
     get 'welcome/index'
   end
+
+  devise_for :users
+  devise_for :admins
 
   root to: 'site/welcome#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+ 
